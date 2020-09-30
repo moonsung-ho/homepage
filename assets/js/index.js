e376931d8338bacf5f70471924d2472d877f1cbc
sungho, moon <moon7959@kakao.com>
2020-09-30 12:10:17 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -120,15 +120,13 @@ function darkmode() {
     "0 0 30px -15px #111111";
 }
 this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-*/
+
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = 'https://79187959.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
-</script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 load();
